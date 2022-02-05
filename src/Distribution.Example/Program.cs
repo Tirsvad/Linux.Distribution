ca7344396843cb89c1b8dc5c1a31f7365effe380
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2022-02-05 14:07:55 +0100

    Added Example of use

---
+++ src/Distribution.Example/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Metadata;
+using System;
+using TirsvadCLI.Linux;
+
+namespace Example;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Distribution use of library Example");
+            Console.WriteLine("Distribion is " + Distribution.DistributionName);
+        }
+}
