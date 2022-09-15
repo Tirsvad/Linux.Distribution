f2a6e27a98c619336e927259233d675a12bbf99e
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2022-09-15 09:41:05 +0200

    guides for GITHUB PAT

--- publish.sh
+++ publish.sh
@@ -8,7 +8,8 @@ JSONFILE="${SCRIPTPATH}/src/${PROJECTDIR}/obj/project.assets.json"
 dotnet build --configuration Release
 dotnet pack --configuration Release
 
-# github source add dotnet nuget add source --username USER --password $GITHUBPAT --store-password-in-clear-text --name github "https://nuget.pkg.github.com/TirsvadCLI/index.json"
-# GITHUBPAT is my secret key for github
+# Github source add: dotnet nuget add source --username USER --password $GITHUBPAT --store-password-in-clear-text --name github "https://nuget.pkg.github.com/TirsvadCLI/index.json"
+# where USER is your github username and $GITHUBPAT is your password. See more at https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+# In this environment the password value is exported so it can be accessed from script
 dotnet nuget push "src/${PROJECTDIR}/bin/Release/$(jq -r '.project.restore.projectName' $JSONFILE).$(jq -r '.project.version' $JSONFILE).nupkg"  --api-key $GITHUBPAT --source https://nuget.pkg.github.com/TirsvadCLI/index.json --skip-duplicate
 dotnet nuget push "src/${PROJECTDIR}/bin/Release/$(jq -r '.project.restore.projectName' $JSONFILE).$(jq -r '.project.version' $JSONFILE).nupkg"  --api-key $NUGETPAT --source https://api.nuget.org/v3/index.json --skip-duplicate
